c8f3b53ea10356f478f5cb275493b899ca3bb57a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-11 14:06:58 -0500

    feat: clase 2: analisis de la estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `<h1>Hola Mundo Sin TemplateUrl</h1>`,
+  // styleUrls: ['./app.component.css'],
+  styles: ['h1 { color: green }'],
 })
 export class AppComponent {
   title = 'bancolombia';
