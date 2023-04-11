f306ad3504e70bac0ed7aee639a0029568954d41
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-11 14:44:41 -0500

    feat: manual add componente-tres

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponenteUnoComponent } from './componente-uno/componente-uno.component';
+import { ComponenteDosComponent } from './componente-dos/componente-dos.component';
+import { ComponenteTresComponent } from './componente-tres/componente-tres.component';
 
 @NgModule({
-  declarations: [AppComponent, ComponenteUnoComponent],
+  declarations: [
+    AppComponent,
+    ComponenteUnoComponent,
+    ComponenteDosComponent,
+    ComponenteTresComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/componente-tres/componente-tres.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-tres',
+  templateUrl: './componente-tres.component.html',
+  styleUrls: ['./componente-tres.component.css'],
+})
+export class ComponenteTresComponent {}
